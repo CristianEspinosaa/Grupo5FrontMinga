1697ea13efab7f089e9b4d2bdb6b179406f3a21e
Jammer <jammer.08123@gmail.com>
2024-11-28 19:51:08 -0600

    se completo la logninpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx'
+import LoginPage from './Pages/lognin_page.jsx'
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
-      <Navbar>
-
-      </Navbar>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
