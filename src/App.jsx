5dca6c1ea7334629674e93a96aea588ef2c2e844
Jammer <jammer.08123@gmail.com>
2024-11-30 17:40:46 -0600

    se agrego pagina manga con autentificacion

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from './Pages/lognin_page.jsx'
 import SignupPage from './Pages/lognup_page.jsx'
 import './App.css'
 import MangasPage from './Pages/mangas_page.jsx'
+import MangasPageAuth from './Pages/mag_page_auth.jsx'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/mangas" element={<MangasPage />} />
+        <Route path="/manga" element={<MangasPageAuth />} />
       </Routes>
     </BrowserRouter>
   )
