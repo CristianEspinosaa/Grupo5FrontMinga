82dbf3aceb160fad4d56543e70b4b4b3e75629b4
CristianE <cristianespinosabarajas@gmail.com>
2024-12-02 17:37:08 -0500

    pagina newRole

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar.jsx'
 import LoginPage from './Pages/lognin_page.jsx'
 import SignupPage from './Pages/lognup_page.jsx'
-import './App.css'
 import MangasPage from './Pages/mangas_page.jsx'
 import MangasPageAuth from './Pages/mag_page_auth.jsx'
+import NewRole from './Pages/NewRole.jsx';
+import 'typeface-roboto';
+import './App.css'
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/mangas" element={<MangasPage />} />
         <Route path="/manga" element={<MangasPageAuth />} />
+        <Route path="/newRole" element={<NewRole />} />
       </Routes>
     </BrowserRouter>
   )
--- src/Pages/NewRole.jsx
+++ src/Pages/NewRole.jsx
@@ -1,60 +1,145 @@
-import React from 'react';
-import rectangle8 from '../assets/Rectangle8.png'
+import React, { useState } from 'react';
+import rectangle8 from '../assets/Rectangle8.png';
+import logMinga from '../assets/log_minga.jpg';
+import img1 from '../assets/author-1.png';
+import img2 from '../assets/author-2.png';
+import img3 from '../assets/author-3.png';
+import img4 from '../assets/company-1.png';
+import img5 from '../assets/company-2.png';
+import img6 from '../assets/company-3.png';
 
-const RoleSelection = () => {
+const NewRole = () => {
+
+  
+  const [selectedRole, setSelectedRole] = useState(null);
+
+  
+  const handleSelectRole = (role) => {
+    setSelectedRole(role === selectedRole ? null : role);
+  };
+
   return (
     <div className="flex flex-col lg:flex-row h-screen justify-center">
-
-      
       <div className="w-full lg:w-1/2 flex items-center justify-center ">
-        <div className="max-w-md p-8 items-center justify-center">
-          <h2 className="text-3xl font-bold text-center mb-8">Change role to</h2>
+        <div className="max-w-lg p-8 items-center justify-center">
+          <h2 className="text-2xl font-roboto text-center mb-2 text-[#4338CA]">Change role to</h2>
           <div className="flex justify-center mb-8">
-            <span className="text-4xl font-extrabold text-indigo-600">MINGA</span>
+            <img src={logMinga} alt="logMinga" className="w-48" />
           </div>
           <div className="space-y-4">
-           
-            <button className="w-full flex items-center justify-between p-4 border rounded-lg hover:border-indigo-600">
-              <div className="flex items-center">
-                <img
-                  src="https://via.placeholder.com/40" 
-                  alt="Author"
-                  className="rounded-full mr-4"
-                />
-                <span className="text-lg font-medium">Join as an Author!</span>
+            
+            <div //Autor
+              onClick={() => handleSelectRole(1)} // Selección de Autor
+              className={`flex items-center justify-between border ${selectedRole === 1 ? "border-blue-600" : "border-gray-300"}
+                rounded-md p-3 gap-4 hover:shadow-md cursor-pointer w-full max-w-md`}
+            >
+              <div className="flex items-center gap-4">
+                <div className="flex -space-x-3">
+                  <img src={img1} alt="Author 1" className="w-9 h-9 rounded-full" />
+                  <img
+                    src={img2}
+                    alt="Author 2"
+                    className="w-11 h-11 rounded-full z-10 scale-105"
+                  />
+                  <img src={img3} alt="Author 3" className="w-9 h-9 rounded-full" />
+                </div>
+                <div>
+                  <span className="text-[#4338CA] font-bold text-lg font-roboto">
+                    Join as an Author!
+                  </span>
+                  <p className="text-sm font-roboto text-[#4338CA]">I'm a reader writing a manga</p>
+                </div>
               </div>
-              <span className="text-sm text-gray-500">I'm a reader writing a manga</span>
-            </button>
-            <button className="w-full flex items-center justify-between p-4 border border-indigo-600 bg-indigo-100 rounded-lg">
-              <div className="flex items-center">
-                <img
-                  src="https://via.placeholder.com/40" 
-                  alt="Company"
-                  className="rounded-full mr-4"
-                />
-                <span className="text-lg font-medium text-indigo-600">Join as a Company!</span>
+
+              <div
+                className={`w-6 h-6 flex items-center justify-center rounded-full ${selectedRole === 1 ? "bg-[#4338CA]" : "bg-white border-gray-300"}`}
+              >
+                {selectedRole === 1 ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="white"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                ) : (
+                  <div className="w-3 h-3 rounded-full bg-slate-300"></div>
+                )}
               </div>
-              <span className="text-sm text-indigo-600">I'm a company and I want to publish my comics</span>
-            </button>
+            </div>
+
+            <div // seleccion empresa
+              onClick={() => handleSelectRole(2)}
+              className={`flex items-center justify-between border ${selectedRole === 2 ? "border-[#4338CA]" : "border-gray-300"}
+                rounded-md p-3 gap-2 hover:shadow-md cursor-pointer`}
+            >
+              <div className="flex items-center gap-4">
+                <div className="flex -space-x-3">
+                  <img src={img4} alt="Company 1" className="w-9 h-9 rounded-full" />
+                  <img
+                    src={img5}
+                    alt="Company 2"
+                    className="w-11 h-11 rounded-full z-10 scale-105"
+                  />
+                  <img src={img6} alt="Company 3" className="w-9 h-9 rounded-full" />
+                </div>
+                <div>
+                  <span className="text-[#4338CA] font-bold text-lg font-roboto">
+                    Join as a Company!
+                  </span>
+                  <p className="text-sm font-roboto text-[#4338CA]">I'm a company and i want to publish my comics</p>
+                </div>
+              </div>
+
+              <div
+                className={`w-6 h-6 flex items-center justify-center rounded-full ${selectedRole === 2 ? "bg-[#4338CA]" : "bg-white border-gray-300"}`}
+              >
+                {selectedRole === 2 ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="white"
+                    className="w-4 h-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                ) : (
+                  <div className="w-3 h-3 rounded-full bg-slate-300"></div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="hidden lg:flex lg:w-1/2 relative">
         <img
           src={rectangle8}
           alt="Background"
           className="w-full h-full object-fill"
         />
-        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center px-10">
-          <h1 className="text-4xl font-extrabold mb-4">Minga.com</h1>
-          <p className="text-lg mb-6">
+        <div className="absolute mt-44 ml-5 font-roboto font-semibold flex flex-col justify-center items-start text-white text-center px-2">
+          <p className="text-2xl mb-6 text-start">
             Minga.com is the best place to find manga reviews. We've been super impressed by the quality of applicants.
           </p>
-          <span className="italic text-sm">Ignacio Borasz</span>
+          <span className="font-roboto font-light text-lg"> - Ignacio Borasz</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default RoleSelection;
+export default NewRole;
