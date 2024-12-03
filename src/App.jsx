0a34b8eaa9d7f857a643125a91c10c93fd5d28f4
CristianE <cristianespinosabarajas@gmail.com>
2024-12-02 21:55:28 -0500

    pagina editAuthor

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignupPage from './Pages/lognup_page.jsx'
 import MangasPage from './Pages/mangas_page.jsx'
 import MangasPageAuth from './Pages/mag_page_auth.jsx'
 import NewRole from './Pages/NewRole.jsx';
+import EditAuthor from './Pages/EditAuthor.jsx'
 import 'typeface-roboto';
 import './App.css'
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/mangas" element={<MangasPage />} />
         <Route path="/manga" element={<MangasPageAuth />} />
         <Route path="/newRole" element={<NewRole />} />
+        <Route path="/editAuthor" element={<EditAuthor />} />
       </Routes>
     </BrowserRouter>
   )
