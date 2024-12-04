eb147a67e2209da2e14a6c4fd994532156e85fef
Jammer <jammer.08123@gmail.com>
2024-12-03 20:46:31 -0600

    se agrego panel de administrador

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import SignupPage from './Pages/lognup_page.jsx'
 import MangasPage from './Pages/mangas_page.jsx'
 import MangasPageAuth from './Pages/mag_page_auth.jsx'
 import NewRole from './Pages/NewRole.jsx';
-import 'typeface-roboto';
+import EditAuthor from './Pages/EditAuthor.jsx'
+import EditChapter from './Pages/EditChapter.jsx'
+import AdminPanel from './Pages/adminPanel.jsx';
 import './App.css'
 
 function App() {
@@ -21,6 +23,7 @@ function App() {
         <Route path="/newRole" element={<NewRole />} />
         <Route path="/editAuthor" element={<EditAuthor />} />
         <Route path="/editChapter" element={<EditChapter />} />
+        <Route path="/adminPanel" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
   )
