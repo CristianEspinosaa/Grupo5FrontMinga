dc85156e385707f5f3d7006191add1012f0516cb
mxsrxm <mxsrxm@gmail.com>
2024-12-06 16:07:48 -0600

    Borrador del Carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Hero from "../Components/Hero.jsx";
+import Carousel from "../Components/Carousel.jsx";
 
 
 const Home = () => {
   return (
     <>
           <Hero></Hero>
+
+          <div className="container mx-auto px-12 py-8">
+               <Carousel></Carousel>
+          </div>
      
     </>
   );
